3e9c5fa86d9589efdde59bf23d151bd46e2b6d72
MuhammadNidal <mnidal414@gmail.com>
2025-06-22 15:30:04 +0500

    dom

--- js/06/index.js
+++ js/06/index.js
@@ -1,7 +1,7 @@
-// const num1 =[0,1,2,3,4,5,6,7,8]
-// const num2 = num1.map(abd => abd + 3)
-// console.log(num2);
-// console.log(num1);
+const num1 =[0,1,2,3,4,5,6,7,8]
+const num2 = num1.map(abd => abd + 3)
+console.log(num2);
+console.log(num1);
 
 
 
---
+++ js/07/index.js
@@ -0,0 +1,34 @@
+// DOM 
+// Documnet get element by id
+
+// const para =document.getElementById("paragrapgh");
+// para.textContent = "hello muhammad nidal"
+// para.style.color = "red"
+// para.style.fontSize=29
+// para.style.fontFamily ="san-serif"
+
+
+// Documnet get element by classsname
+// const heading =document.getElementsByClassName("h1")
+// heading[0].textContent="skill up academy"
+// heading[1].style.color ="pink"
+
+// Documnet get element by tag name
+
+// const heading2 =document.getElementsByTagName("h3")
+// heading2[0].style.color="orange"
+// heading2[1].style.fontFamily="san-serif"
+
+
+// // Documnet get element by query
+
+const heading2 =document.querySelector(".containter")
+heading2.textContent ="helloooo"
+heading2.style.color= "pink"
+
+
+// // Documnet get element by queryselectorall
+
+const notes =document.querySelectorAll(".note")
+notes[0].style.border = "20px solid green"
+notes[1].style.color ="yellow"
